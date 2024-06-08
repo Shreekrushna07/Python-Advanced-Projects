57f26931cd2027e9de26b35855a6803e38d57faf
Shreekrushna Ravindra Dongare <160056017+Shreekrushna07@users.noreply.github.com>
2024-06-08 20:39:15 +0530

    Add files via upload

---
+++ Snake Game/scoreboard.py
@@ -0,0 +1,38 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def reset(self):
+        self.clear()
+        self.score = 0
+        self.goto(0, 270)  
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=("Courier", 24, "normal"))
+        self.goto(0, -30)
+        self.write("Press 'r' to retry", align="center", font=("Courier", 18, "normal"))
